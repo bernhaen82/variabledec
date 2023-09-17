29a11bf0ce5518a7df5afad9241c2967954cdb55
bernhaen82 <144837979+bernhaen82@users.noreply.github.com>
2023-09-17 13:10:50 -0400

    Add files via upload

---
+++ variabedec/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static <string> void main(String[] args) {
+        int numberKids= 3;
+        String isItRaining = ( "It is not raining ");
+        double gallonOfGas= 3.69;
+        int yourFavNum = 8;
+        String birthMonth = ("November");
+        String myName = ("My name is Ethan Bernhardt");
+        System.out.println(" The number of kids in my family is " + numberKids);
+        System.out.println(" Is it Raining? " + isItRaining);
+        System.out.println(" The price of a gallon of gas is " + gallonOfGas);
+        System.out.println(" My favorite number is  " + yourFavNum);
+        System.out.println(" My birth month is  " + birthMonth);
+        System.out.println(" My full name is  " + myName);
+    }
+}
